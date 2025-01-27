99e02f6803e6ac0093877b61696f6f1cd136520c
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2025-01-27 20:43:18 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,55 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Process {
-    int id;         
-    int arrival_time;
-    int burst_time;   
-    int waiting_time; 
+typedef struct {
+    int pid;             
+    int arrival_time;   
+    int burst_time;      
+    int completion_time; 
     int turnaround_time; 
-};
+    int waiting_time;    
+    int response_time;   
+} Process;
 
 int compare(const void *a, const void *b) {
-    struct Process *p1 = (struct Process *)a;
-    struct Process *p2 = (struct Process *)b;
-    return p1->burst_time - p2->burst_time;
+    Process *p = (Process*)a, *q = (Process*)b;
+    return (p->arrival_time != q->arrival_time) ? 
+           p->arrival_time - q->arrival_time : 
+           p->burst_time - q->burst_time;
 }
 
-void calculateTimes(struct Process processes[], int n) {
-    processes[0].waiting_time = 0;
-    processes[0].turnaround_time = processes[0].burst_time;
-
-    for (int i = 1; i < n; i++) {
-        processes[i].waiting_time = processes[i-1].waiting_time + processes[i-1].burst_time;
-        processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
-    }
-}
-
-void printProcesses(struct Process processes[], int n) {
-    printf("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].id, processes[i].arrival_time, processes[i].burst_time, processes[i].waiting_time, processes[i].turnaround_time);
+void calculateTimes(Process p[], int n) {
+    int current = 0, completed = 0;
+    while(completed < n) {
+        int next = -1, min_burst = 1e9;
+        
+        for(int i = 0; i < n; i++) {
+            if(p[i].arrival_time <= current && p[i].waiting_time == -1 && p[i].burst_time < min_burst) {
+                min_burst = p[i].burst_time;
+                next = i;
+            }
+        }
+        
+        if(next == -1) {
+            int next_arrival = 1e9;
+            for(int i = 0; i < n; i++) {
+                if(p[i].waiting_time == -1 && p[i].arrival_time < next_arrival) 
+                    next_arrival = p[i].arrival_time;
+            }
+            current = next_arrival;
+        } 
+        else {
+            p[next].completion_time = current + p[next].burst_time;
+            p[next].turnaround_time = p[next].completion_time - p[next].arrival_time;
+            p[next].waiting_time = current - p[next].arrival_time;
+            p[next].response_time = p[next].waiting_time; 
+            current = p[next].completion_time;
+            completed++;
+        }
     }
 }
 
 int main() {
     int n;
-
-    printf("Enter the number of processes: ");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
-
-    struct Process processes[n];
-    for (int i = 0; i < n; i++) {
-        processes[i].id = i + 1;
-        printf("Enter arrival time for process %d: ", i + 1);
-        scanf("%d", &processes[i].arrival_time);
-        printf("Enter burst time for process %d: ", i + 1);
-        scanf("%d", &processes[i].burst_time);
+    Process p[n];
+    
+    for(int i = 0; i < n; i++) {
+        printf("Process %d (PID, Arrival, Burst): ", i+1);
+        scanf("%d%d%d", &p[i].pid, &p[i].arrival_time, &p[i].burst_time);
+        p[i].waiting_time = -1; 
     }
 
-    qsort(processes, n, sizeof(struct Process), compare);
-    calculateTimes(processes, n);
-    printProcesses(processes, n);
+    qsort(p, n, sizeof(Process), compare);
+    calculateTimes(p, n);
 
+    float avg_turnaround = 0, avg_waiting = 0, avg_response = 0, avg_completion=0;
+    printf("\n%-10s %-12s %-10s %-12s %-12s %-12s %-12s\n", "PID", "Arrival", "Burst", "Response", "Completion", "Turnaround", "Waiting");
+    for(int i = 0; i < n; i++) {
+        printf("%-10d %-12d %-10d %-12d %-12d %-12d %-12d\n", 
+              p[i].pid, 
+              p[i].arrival_time,
+              p[i].burst_time,
+              p[i].response_time,
+              p[i].completion_time,
+              p[i].turnaround_time,
+              p[i].waiting_time
+            );
+        avg_turnaround += p[i].turnaround_time;
+        avg_waiting += p[i].waiting_time;
+        avg_response += p[i].response_time;
+        avg_completion += p[i].completion_time;
+    }
+
+    printf("\nAverage:");
+    printf("\nResponse Time:   %f", avg_response/n);
+    printf("\nCompletion Time: %f", avg_completion/n);
+    printf("\nTurnaround Time: %f", avg_turnaround/n);
+    printf("\nWaiting Time:    %f\n", avg_waiting/n);
     return 0;
 }
