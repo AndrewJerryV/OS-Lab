a7e0f9ca96bc45b5a17697c7e8ba2a92ad6fcf3b
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2025-01-30 14:22:58 +0530

    Create semaphores(implementation).c

---
+++ semaphores(implementation).c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+typedef struct {
+    int value;
+} semaphore;
+
+void semaphore_wait(semaphore *s) {
+    while (s->value <= 0);
+    s->value--;
+}
+
+void semaphore_signal(semaphore *s) {
+    s->value++;
+}
+
+int main() {
+    semaphore *sem = mmap(NULL, sizeof(semaphore), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (sem == MAP_FAILED) {
+        perror("mmap failed");
+        exit(1);
+    }
+    
+    sem->value = 1; 
+    
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    }
+    
+    if (pid == 0) {
+        semaphore_wait(sem);
+        printf("Child process entered critical section.\n");
+        sleep(2);
+        printf("Child process leaving critical section.\n");
+        semaphore_signal(sem);
+        exit(0);
+    } 
+    else {
+        semaphore_wait(sem);
+        printf("Parent process entered critical section.\n");
+        sleep(2);
+        printf("Parent process leaving critical section.\n");
+        semaphore_signal(sem);
+        wait(NULL);
+    }
+    
+    munmap(sem, sizeof(semaphore));
+    return 0;
+}
