4c0aaa006529a55db2470eb8c7861e608ec74a94
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2025-03-06 13:07:28 +0530

    Update page_replacement_algorithms.c
    Added mfu and optimal algorithms

--- page_replacement_algorithms.c
+++ page_replacement_algorithms.c
@@ -112,6 +112,97 @@ void lfu(int pages[], int n, int frames) {
     printf("Total LFU Page Faults: %d\n", page_faults);
 }
 
+void mfu(int pages[], int n, int frames) {
+    int frame[frames], freq[frames], fifo[frames], page_faults = 0;
+    
+    for (int i = 0; i < frames; i++) {
+        frame[i] = -1;
+        freq[i] = 0;
+        fifo[i] = -1;
+    }
+    
+    printf("\nMFU Page Replacement:\n");
+    for (int i = 0; i < n; i++) {
+        bool found = false;
+        for (int j = 0; j < frames; j++) {
+            if (frame[j] == pages[i]) {
+                found = true;
+                freq[j]++; 
+                break;
+            }
+        }
+        if (!found) {
+            int mfu_index = -1;
+            for (int j = 0; j < frames; j++) {
+                if (frame[j] == -1) {
+                    mfu_index = j;
+                    break;
+                }
+            }
+            if (mfu_index == -1) {
+                int max_freq = -1, oldest = 99999;
+                for (int j = 0; j < frames; j++) {
+                    if (freq[j] > max_freq) {
+                        max_freq = freq[j];
+                        mfu_index = j;
+                    }
+                }
+            }
+            frame[mfu_index] = pages[i];
+            freq[mfu_index] = 1;     
+            page_faults++;
+        }
+        displayframes(frame, frames);
+    }
+    printf("Total MFU Page Faults: %d\n", page_faults);
+}
+
+void optimal(int pages[], int n, int frames) {
+    int frame[frames], page_faults = 0;
+    for (int i = 0; i < frames; i++) frame[i] = -1;
+    printf("\nOptimal Page Replacement:\n");
+    for (int i = 0; i < n; i++) {
+        bool found = false;
+        for (int j = 0; j < frames; j++) {
+            if (frame[j] == pages[i]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            int pos = -1;
+            for (int j = 0; j < frames; j++) {
+                if (frame[j] == -1) {
+                    pos = j;
+                    break;
+                }
+            }
+            if (pos == -1) {
+                int farthest = -1;
+                for (int j = 0; j < frames; j++) {
+                    int k;
+                    for (k = i + 1; k < n; k++) {
+                        if (frame[j] == pages[k])
+                            break;
+                    }
+                    if (k == n) {
+                        pos = j;
+                        break;
+                    }
+                    if (k > farthest) {
+                        farthest = k;
+                        pos = j;
+                    }
+                }
+            }
+            frame[pos] = pages[i];
+            page_faults++;
+        }
+        displayframes(frame, frames);
+    }
+    printf("Total Optimal Page Faults: %d\n", page_faults);
+}
+
 int main() {
     int n, frames;
     printf("Enter number of pages: ");
@@ -125,6 +216,8 @@ int main() {
     fifo(pages, n, frames);
     lru(pages, n, frames);
     lfu(pages, n, frames);
+    mfu(pages, n, frames);
+    optimal(pages, n, frames);
     
     return 0;
 }
